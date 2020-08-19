46aee30bee664c0fba7b1f2a8dd48719cd57576a
Slinkter <luis.j.cueva@gmail.com>
2020-08-19 12:00:54 -0500

    se genera email , y se envia correo el pdf

--- app/src/main/java/com/cudpast/apputdemo/Support/ShowPdfActivity.java
+++ app/src/main/java/com/cudpast/apputdemo/Support/ShowPdfActivity.java
@@ -7,12 +7,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.cudpast.apputdemo.Common.Common;
 import com.cudpast.apputdemo.R;
 import com.github.barteksc.pdfviewer.PDFView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.File;
 
@@ -22,6 +24,7 @@ public class ShowPdfActivity extends AppCompatActivity {
     private File file;
 
     public static final String TAG = ShowPdfActivity.class.getSimpleName();
+    public static final String folderpdf = "/arsi21.pdf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,69 +32,56 @@ public class ShowPdfActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_show_pdf);
-        pdfView = findViewById(R.id.pdfView);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendEmail();
+            }
+        });
+        pdfView = findViewById(R.id.pdfView2);
         init();
 
 
     }
+
     private void init() {
         try {
-            file = new File(Environment.getExternalStorageDirectory(), "/arsi21.pdf");
-            Log.e(TAG, "file archivo  " + file.toString());
+            file = new File(Environment.getExternalStorageDirectory(), folderpdf);
             pdfView.fromFile(file)
                     .enableSwipe(true)
                     .swipeHorizontal(false)
                     .enableDoubletap(true)
                     .enableAntialiasing(true)
                     .load();
-
-
-            //    sendEmail2();
-
+            //
+            Log.e(TAG, "file archivo  " + file.toString());
         } catch (Exception e) {
-            e.printStackTrace();
+            Log.e(TAG, " error: " + e.getMessage());
         }
     }
 
-
-    private void sendEmail2() {
-        Log.e(TAG, "sendEmail()  2 ");
+    private void sendEmail() {
+        //
+        String currentusermail = Common.currentUser.getEmail();
+        String message = "Documento Generado por " + Common.currentUser.getName() + "\n Saludos";
         File root = Environment.getExternalStorageDirectory();
-        String filelocation = root.getAbsolutePath() + "/arsi21.pdf";
+        String filelocation = root.getAbsolutePath() + folderpdf;
+        //
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setType("text/plain");
-        String message = "Documento Generado por " + Common.currentUser.getName();
-        intent.putExtra(Intent.EXTRA_SUBJECT, "Unidades ARSI : " + Common.unidadTrabajoSelected.getAliasUT() + "\n Saludos");
-        intent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + filelocation));
+        //  intent.setDataAndType(Uri.parse("file://" + filelocation),"text/plain");
+        intent.setData(Uri.parse("mailto:" + currentusermail));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Unidades ARSI : " + Common.unidadTrabajoSelected.getNameUT());
         intent.putExtra(Intent.EXTRA_TEXT, message);
-        intent.setData(Uri.parse("mailto:luis.j.cueva@gmail.com"));
+        intent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + filelocation));
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        Log.e(TAG, "sendEmail 2  -->  filelocation " + filelocation);
+        //
+        Log.e(TAG, "sendEmail()");
+        Log.e(TAG, "currentusermail  : " + currentusermail);
+        Log.e(TAG, "sendEmail : filelocation " + filelocation);
         startActivity(intent);
     }
 
-
-    private void initApp() {
-
-        file = new File(Environment.getExternalStorageDirectory(), "/arsi21.pdf");
-        Log.e(TAG, "file archivo initApp() " + file.toString());
-        if (file.exists()) {
-            try {
-                Log.e(TAG, "archivo existe ");
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setDataAndType(Uri.fromFile(file), "application/pdf");
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                Log.e(TAG, "1 .. 2");
-                this.startActivity(intent);
-            } catch (Exception e) {
-                Log.e(TAG, e.getMessage());
-                this.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.adobe.reader&hl=en")));
-                Toast.makeText(this, "no cuenta con una aplicacion de pdf", Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            Toast.makeText(this, "no cuenta con una aplicacion de pdf", Toast.LENGTH_SHORT).show();
-        }
-
-    }
 }
