6f6708dd002af9cb337785879919d871969e64a0
arsi <consultingarsiapp@gmail.com>
2020-11-12 14:28:58 -0500

    big sur

---
+++ app/src/main/java/com/cudpast/apputdemo/Main/GraphicReport.java
@@ -0,0 +1,17 @@
+package com.cudpast.apputdemo.Main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cudpast.apputdemo.R;
+
+public class GraphicReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graphic_report);
+
+    }
+}
